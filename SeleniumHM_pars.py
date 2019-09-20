4ba222abcd4c5d0dd1eda382990b2058be59f0be
kilokokaina <nikolaushki@gmail.com>
2019-09-20 12:24:37 +0800

    Update SeleniumHM_pars.py

--- SeleniumHM_pars.py
+++ SeleniumHM_pars.py
@@ -2,26 +2,35 @@ from selenium import webdriver
 from time import sleep
 
 class Bot :
+    global URL, new_URL
+    URL = 'https://www2.hm.com/ru_ru/zhenshchiny/novye-postupleniya/odezhda.html'
+
     def __init__( self ) :
         self.driver = webdriver.Firefox()
         self.navigate()
 
     def navigate( self ) :
-        self.driver.get('https://www2.hm.com/ru_ru/zhenshchiny/novye-postupleniya/odezhda.html')
+        self.driver.get(URL)
 
         button = self.driver.find_element_by_xpath('//button[@class="button js-load-more "]')
 
         while True :
             try :
                 button.click()
-                sleep(5)
+                sleep(3)
             except :
                 break
 
         print('I`m done!)')
+
+        file = open('url.txt', 'w')
+
+        file.write(self.driver.current_url)
+        file.close()
 
 def main() :
     b = Bot()
 
 if __name__ == '__main__' :
     main()
+
